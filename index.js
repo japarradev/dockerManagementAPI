ece41f9bf833cd0a28959aa658f6e2cbaa06f0d3
japarradev <j.parra.903@gmail.com>
2024-12-06 04:51:47 -0500

    Ajustes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // index.js
-const express = require('express');
-require('dotenv').config();
+const express = require('express')
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 // Middleware to parse JSON bodies
-app.use(express.json());
+app.use(express.json())
 
 // Import routes
-const dataRoutes = require('./routes/dataRoutes');
+const dataRoutes = require('./routes/dataRoutes')
 
 // Use the routes
-app.use('/api/v1', dataRoutes);
+app.use('/api/v1', dataRoutes)
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
